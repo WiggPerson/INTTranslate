9b78684ed16ce34df3344f107a090187d792fe39
Anton Lithuan <wiggperson@gmail.com>
2014-10-04 12:02:50 +0200

    Add chat integration

--- src/eu/intezzy/inttranslate/Events.java
+++ src/eu/intezzy/inttranslate/Events.java
@@ -1,9 +1,16 @@
 package eu.intezzy.inttranslate;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import eu.intezzy.inttranslate.api.GeoIP;
@@ -46,6 +53,39 @@ public class Events implements Listener {
 		
 		e.getPlayer().sendMessage(ChatColor.GREEN + "[INTTranslate] " + ChatColor.WHITE + new Translate("sk",Main.data.getPlayerLang(e.getPlayer()).toLowerCase(), "Tvôj jazyk bol nastavený na " + new Translate("en","sk", Main.lang.languages.get(Main.data.getPlayerLang(e.getPlayer()))).translate()).translate() + ".");
 		
+	}
+	
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent e) {
+		
+		System.out.println("ha");
+		
+		String msg = e.getMessage();
+		
+		if(msg.startsWith("/")) return;
+		
+		HashMap<Player, String> messages = new HashMap<Player, String>();
+		
+		for(Player p : Bukkit.getOnlinePlayers()) {
+			String playerMessage = new String("");
+			if(Main.data.getPlayerLang(e.getPlayer()).equals(Main.data.getPlayerLang(p))) {
+				System.out.println("ha");
+				playerMessage = ChatColor.WHITE + "<" + p.getDisplayName() + ChatColor.WHITE + "> " + msg;
+			} else {			
+				playerMessage = ChatColor.WHITE + "<" + p.getDisplayName() + ChatColor.WHITE + "> " + new Translate(Main.data.getPlayerLang(e.getPlayer()), Main.data.getPlayerLang(p), msg).translate();
+			}
+			messages.put(p, playerMessage);
+		}
+		
+		for(Map.Entry<Player, String> entry : messages.entrySet()) {
+			Player p = entry.getKey();
+			String msg_ = entry.getValue();
+			
+			p.sendMessage(msg_);
+		}
+		
+		System.out.println("ha");
+		e.setCancelled(true);
 	}
 	
 }
